02da026277c0efbced020cab83493d008b98caec
Denis <deniszulfikar7@gmail.com>
2026-03-03 19:23:14 +0700

    feat(flex): add fstring_view API with slicing, trimming, comparison, and advancing utilities

--- flex.c
+++ flex.c
@@ -3,6 +3,28 @@
 
 #include "flex.h"
 
+#define MIN(a, b) ((a < b) ? a : b)
+#define MAX(a, b) ((a > b) ? a : b)
+
+static inline int is_spaces(char ch) {
+    return ch == '\t' || ch == '\r' || ch == '\n' || ch == ' ' || ch == '\v' || ch == '\f';
+}
+
+fstring flex_str_from_sv(fstring_view sv) {
+    fstring fstr = { 0 };
+    if (sv.len > 0) {
+        void *data = malloc(sv.len);
+        if (!data)
+            return fstr;
+
+        memcpy(data, sv.data, sv.len);
+        fstr.data = data;
+        fstr.len = sv.len;
+    }
+
+    return fstr;
+}
+
 fstring flex_str_from_cstr(const char *cstr) {
     fstring fstr = { 0 };
     size_t n = strlen(cstr);
@@ -23,6 +45,13 @@ void flex_str_free(fstring *str) {
     free((void*)str->data);
     str->data = NULL;
     str->len = 0;
+}
+
+fstring_view flex_str_slice(fstring str, size_t start, size_t end) {
+    end = MIN(end, str.len);
+    start = MIN(start, end);
+
+    return FLEX_SV_BUF(str.data + start, end - start);
 }
 
 fstring flex_str_toupper(fstring str) {
@@ -53,4 +82,211 @@ fstring flex_str_tolower(fstring str) {
     }
 
     return (fstring) { new_data, str.len };
+}
+
+fstring_view flex_sv_slice(fstring_view sv, size_t start, size_t end) {
+    end = MIN(end, sv.len);
+    start = MIN(start, end);
+
+    return FLEX_SV_BUF(sv.data + start, end - start);
+}
+
+fstring_view flex_sv_ltrim(fstring_view sv) {
+    if (sv.len == 0)
+        return FLEX_SV_EMPTY;
+
+    size_t i = 0;
+    while (i < sv.len && is_spaces(sv.data[i]))
+        i++;
+
+    return FLEX_SV_BUF(sv.data + i, sv.len - i);
+}
+
+fstring_view flex_sv_rtrim(fstring_view sv) {
+    if (sv.len == 0)
+        return FLEX_SV_EMPTY;
+
+    size_t i = 0;
+    while (i < sv.len && is_spaces(sv.data[sv.len - 1 - i]))
+        i++;
+
+    return FLEX_SV_BUF(sv.data, sv.len - i);
+}
+
+fstring_view flex_sv_trim(fstring_view sv) {
+    if (sv.len == 0)
+        return FLEX_SV_EMPTY;
+
+    return flex_sv_rtrim(flex_sv_ltrim(sv));
+}
+
+fstring_view flex_sv_take_n(fstring_view sv, size_t n) {
+    n = MIN(n, sv.len);
+    return FLEX_SV_BUF(sv.data, n);
+}
+
+fstring_view flex_sv_take_while(fstring_view sv, bool (*predicate)(char x)) {
+    if (sv.len == 0)
+        return FLEX_SV_EMPTY;
+
+    size_t i = 0;
+    while (i < sv.len && predicate(sv.data[i]))
+        i++;
+
+    return FLEX_SV_BUF(sv.data, i); // Not including th last char
+}
+
+fstring_view flex_sv_take_until(fstring_view sv, char x) {
+    if (sv.len == 0)
+        return FLEX_SV_EMPTY;
+
+    size_t i = 0;
+    while (i + 1 <= sv.len && sv.data[i] != x)
+        i++;
+
+    fstring_view res = FLEX_SV_EMPTY;
+    if (i + 1 <= sv.len)
+        res = FLEX_SV_BUF(sv.data, i + 1);
+
+    return res;
+}
+
+fstring_view flex_sv_take_until_sv(fstring_view sv, fstring_view predicate) {
+    if (sv.len == 0)
+        return FLEX_SV_EMPTY;
+
+    fstring_view window = FLEX_SV_BUF(sv.data, predicate.len);
+    size_t i = 0;
+    while (i + predicate.len <= sv.len && !flex_sv_equal(window, predicate))
+        i++, window.data++;
+
+    fstring_view res = FLEX_SV_EMPTY;
+    if (i + predicate.len <= sv.len)
+        res = FLEX_SV_BUF(sv.data, i + predicate.len);
+
+    return res;
+}
+
+fstring_view flex_sv_adv_n(fstring_view *sv, size_t n) {
+    n = MIN(n, sv->len);
+
+    fstring_view res = FLEX_SV_BUF(sv->data, n);
+    sv->data += n;
+    sv->len -= n;
+
+    return res;
+}
+
+fstring_view flex_sv_adv_while(fstring_view *sv, bool (*predicate)(char x)) {
+    if (sv->len == 0)
+        return FLEX_SV_EMPTY;
+
+    size_t i = 0;
+    while (i < sv->len && predicate(sv->data[i]))
+        i++;
+
+    fstring_view res = FLEX_SV_BUF(sv->data, i);
+    sv->data += i;
+    sv->len -= i;
+
+    return res;
+}
+
+fstring_view flex_sv_adv_until(fstring_view *sv, char x) {
+    if (sv->len == 0)
+        return FLEX_SV_EMPTY;
+
+    size_t i = 0;
+    while (i + 1 <= sv->len && sv->data[i] != x)
+        i++;
+
+    fstring_view res = FLEX_SV_EMPTY;
+    if (i + 1 <= sv->len) {
+        res = FLEX_SV_BUF(sv->data, i + 1);
+        sv->data += i + 1;
+        sv->len -= i + 1;
+    }
+
+    return res;
+}
+
+fstring_view flex_sv_adv_until_sv(fstring_view *sv, fstring_view predicate) {
+    if (sv->len == 0)
+        return FLEX_SV_EMPTY;
+
+    fstring_view window = FLEX_SV_BUF(sv->data, predicate.len);
+    size_t i = 0;
+    while (i + predicate.len <= sv->len && !flex_sv_equal(window, predicate))
+        i++, window.data++;
+
+    fstring_view res = FLEX_SV_EMPTY;
+    if (i + predicate.len <= sv->len) {
+        res = FLEX_SV_BUF(sv->data, i + predicate.len);
+        sv->data += i + predicate.len;
+        sv->len -= i + predicate.len;
+    }
+
+    return res;
+}
+
+bool flex_sv_starts_with(fstring_view sv, fstring_view prefix) {
+    if (sv.len >= prefix.len) {
+        fstring_view actual_prefix = FLEX_SV_BUF(sv.data, prefix.len);
+        return flex_sv_equal(sv, actual_prefix);
+    }
+
+    return false;
+}
+
+bool flex_sv_ends_with(fstring_view sv, fstring_view suffix) {
+    if (sv.len >= suffix.len) {
+        fstring_view actual_suffix = FLEX_SV_BUF(sv.data + sv.len - suffix.len, suffix.len);
+        return flex_sv_equal(suffix, actual_suffix);
+    }
+
+    return false;
+}
+
+bool flex_sv_equal(fstring_view lhs, fstring_view rhs) {
+    if (!lhs.data || !rhs.data)
+        return false;
+    if (lhs.len != rhs.len)
+        return false;
+    if (lhs.len == 0)
+        return true;
+
+    return memcmp(lhs.data, rhs.data, lhs.len) == 0;
+}
+
+bool flex_sv_equali(fstring_view lhs, fstring_view rhs) {
+    if (!lhs.data || !rhs.data)
+        return false;
+    if (lhs.len != rhs.len)
+        return false;
+    if (lhs.len == 0)
+        return true;
+
+    char a, b;
+    for (size_t i = 0; i < lhs.len; i++) {
+        uint8_t l = lhs.data[i];
+        uint8_t r = rhs.data[i];
+        a = 'A' <= l && l <= 'Z' ? l + 32 : l;
+        b = 'A' <= r && r <= 'Z' ? r + 32 : r;
+
+        if (a != b)
+            return false;
+    }
+
+    return true;
+}
+
+int flex_sv_index_of(fstring_view sv, char x) {
+    size_t i = 0;
+    while (i < sv.len && sv.data[i] != x)
+        i++;
+
+    if (i < sv.len)
+        return i;
+
+    return -1;
 }
--- flex.h
+++ flex.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define FLEX_STR_FMT(str) (int)(str).len, (str).data
 
@@ -14,10 +15,39 @@ typedef struct {
     size_t len;
 } fstring;
 
+typedef struct {
+    const uint8_t *data;
+    size_t len;
+} fstring_view;
+
+fstring flex_str_from_sv(fstring_view sv);
 fstring flex_str_from_cstr(const char *cstr);
 void flex_str_free(fstring *str);
+fstring_view flex_str_slice(fstring str, size_t start, size_t end);
 fstring flex_str_toupper(fstring str);
 fstring flex_str_tolower(fstring str);
+
+#define FLEX_SV_EMPTY (fstring_view) { NULL, 0 }
+#define FLEX_SV_LIT(cstr) (fstring_view) { (uint8_t*)(cstr), sizeof(cstr) - 1 }
+#define FLEX_SV_STR(str) (fstring_view) { (str).data, (str).len }
+#define FLEX_SV_BUF(data, len) (fstring_view) { (uint8_t*)(data), (len) }
+fstring_view flex_sv_slice(fstring_view sv, size_t start, size_t end);
+fstring_view flex_sv_ltrim(fstring_view sv);
+fstring_view flex_sv_rtrim(fstring_view sv);
+fstring_view flex_sv_trim(fstring_view sv);
+fstring_view flex_sv_take_n(fstring_view sv, size_t n);
+fstring_view flex_sv_take_while(fstring_view sv, bool (*predicate)(char x));
+fstring_view flex_sv_take_until(fstring_view sv, char x);
+fstring_view flex_sv_take_until_sv(fstring_view sv, fstring_view predicate);
+fstring_view flex_sv_adv_n(fstring_view *sv, size_t n);
+fstring_view flex_sv_adv_while(fstring_view *sv, bool (*predicate)(char x));
+fstring_view flex_sv_adv_until(fstring_view *sv, char x);
+fstring_view flex_sv_adv_until_sv(fstring_view *sv, fstring_view predicate);
+bool flex_sv_starts_with(fstring_view sv, fstring_view prefix);
+bool flex_sv_ends_with(fstring_view sv, fstring_view suffix);
+bool flex_sv_equal(fstring_view lhs, fstring_view rhs);
+bool flex_sv_equali(fstring_view lhs, fstring_view rhs);
+int flex_sv_index_of(fstring_view sv, char x);
 
 
 #ifdef __cplusplus
